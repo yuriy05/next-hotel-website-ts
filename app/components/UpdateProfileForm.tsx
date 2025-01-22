94a18d42cb50f35aafa46a70e853f2e9828d575e
yuriy05 <dilayyura@gmail.com>
2025-01-22 21:44:27 +0200

    server actions | updating guest in the supabase

--- app/components/UpdateProfileForm.tsx
+++ app/components/UpdateProfileForm.tsx
@@ -3,8 +3,6 @@
 import Image from "next/image";
 import { updateGuest } from "../lib/actions";
 
-const countryFlag = "/pt.jpg";
-
 function UpdateProfileForm({
   children,
   guest,
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,9 +1,31 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { supabase } from "../services/supabase";
+import { auth, signIn, signOut } from "./auth";
 
-export async function updateGuest(formData: object) {
-  console.log(formData);
+export async function updateGuest(formData: any) {
+  const session = await auth();
+
+  if (!session) throw new Error("You must be logged in");
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error("Please provide a valid national ID");
+
+  const updatedData = {
+    nationality,
+    nationalID,
+    countryFlag,
+  };
+
+  const { data, error } = await supabase
+    .from("Guests")
+    .update(updatedData)
+    .eq("id", session!.user!.guestId);
+
+  if (error) throw new Error("Guest could not be updated");
 }
 
 export async function signInAction() {
