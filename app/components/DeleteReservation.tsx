4f3abecd30dbf5a1dd4f508d351d0cf16da4ffb9
yuriy05 <dilayyura@gmail.com>
2025-01-26 20:59:27 +0200

    use transition for server action | changed and improved UI/UX for modal window

--- app/components/DeleteReservation.tsx
+++ app/components/DeleteReservation.tsx
@@ -3,22 +3,38 @@
 import { TrashIcon } from "@heroicons/react/24/solid";
 import Modal from "./Modal";
 import DeleteReservationConfrim from "./DeleteReservationConfirm";
+import { useTransition } from "react";
+import SpinnerMini from "./SpinnerMini";
 
 type DeleteReservationProps = {
   readonly bookingId: number;
 };
 
 function DeleteReservation({ bookingId }: DeleteReservationProps): JSX.Element {
+  const [isPending, startTransation] = useTransition();
+
   return (
     <Modal>
       <Modal.Open opens="delete-reservation">
         <button className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900">
-          <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
-          <span className="mt-1">Delete</span>
+          {isPending ? (
+            <span className="mx-auto">
+              <SpinnerMini />
+            </span>
+          ) : (
+            <>
+              <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+              <span className="mt-1">Delete</span>
+            </>
+          )}
         </button>
       </Modal.Open>
       <Modal.Window name="delete-reservation">
-        <DeleteReservationConfrim bookingId={bookingId} />
+        <DeleteReservationConfrim
+          isPending={isPending}
+          startTransition={startTransation}
+          bookingId={bookingId}
+        />
       </Modal.Window>
     </Modal>
   );
--- app/components/DeleteReservationConfirm.tsx
+++ app/components/DeleteReservationConfirm.tsx
@@ -1,12 +1,22 @@
+import { TransitionStartFunction, useTransition } from "react";
 import { deleteReservation } from "../lib/actions";
+import SpinnerMini from "./SpinnerMini";
 
 function DeleteReservationConfrim({
   onCloseModal,
   bookingId,
+  startTransition,
+  isPending,
 }: {
   onCloseModal?: () => void;
+  startTransition: TransitionStartFunction;
   bookingId: number;
+  isPending: boolean;
 }): JSX.Element {
+  function handleDelete() {
+    startTransition(() => deleteReservation(bookingId));
+  }
+
   return (
     <div className="flex flex-col space-y-5">
       <h1 className="text-3xl">Delete reservation</h1>
@@ -18,15 +28,23 @@ function DeleteReservationConfrim({
       <div className="flex justify-end gap-5">
         <button
           onClick={onCloseModal}
-          className="py-2 px-3 text-green-600 border-green-600 border hover:bg-green-700 hover:text-green-50 transition-all duration-300"
+          disabled={isPending}
+          className="py-2 px-3 w-[74.6px] text-green-600 border-green-600 border hover:bg-green-700 hover:text-green-50 transition-all duration-300"
         >
           Cancel
         </button>
         <button
-          onClick={() => deleteReservation(bookingId)}
-          className="py-2 px-3 text-red-600 border-red-600 border hover:bg-red-700 hover:text-red-50 transition-all duration-300"
+          onClick={handleDelete}
+          disabled={isPending}
+          className="py-2 px-3 w-[74.6px] text-red-600 border-red-600 border hover:bg-red-700 hover:text-red-50 transition-all duration-300"
         >
-          Delete
+          {isPending ? (
+            <span className="flex justify-center">
+              <SpinnerMini />
+            </span>
+          ) : (
+            "Delete"
+          )}
         </button>
       </div>
     </div>
